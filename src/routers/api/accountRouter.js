cfbf059cbecbcaa40f4b3af38796830836189275
Dongzhu Tan <dt222ha@student.lnu.se>
2024-08-26 11:59:27 +0200

    Register user to the database successful!

--- src/routers/api/accountRouter.js
+++ src/routers/api/accountRouter.js
@@ -6,11 +6,18 @@
  */
 
 import express from 'express'
-
+import { AccountController } from '../../controller/AccountController.js'
 
 export const router = express.Router()
+const accountController = new AccountController()
 
 
 // Map HTTP verbs and route paths to controller action methods.
 
 router.get('/', (req, res) => res.json({ message: '{"message":"Welcome to the second-hand store！"}' }))
+
+// Register
+router.post('/register', (req, res, next) => accountController.register(req, res, next))
+
+// Log in
+router.post('/login', (req, res, next) => accountController.login(req, res, next))
