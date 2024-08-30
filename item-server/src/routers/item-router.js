8df9e9a4e5600ec77d5eb1f67cedfd95904e7bac
Dongzhu Tan <dt222ha@student.lnu.se>
2024-08-30 15:22:17 +0200

    Get all of the users items with path /user/items

--- item-server/src/routers/item-router.js
+++ item-server/src/routers/item-router.js
@@ -17,13 +17,12 @@ router.param('id', (req, res, next, id) => itemsController.loadItemsDocument(req
 
 // Public routes.
 router.get('/', (req, res, next) => itemsController.getAllItems(req, res, next))
-
 router.get('/:itemId', (req, res, next) => itemsController.showItemWithId(req, res, next))
 
 // Protected routes.
-router.post('/create', authenticateJWT, (req, res, next) => itemsController.createItem(req, res, next))
+router.get('/user/items', authenticateJWT, (req, res, next) => itemsController.showAllItemsFromUser(req, res, next))
 
-router.get('/my-items', authenticateJWT, (req, res, next) => itemsController.showAllItemsFromUser(req, res, next))
+router.post('/create', authenticateJWT, (req, res, next) => itemsController.createItem(req, res, next))
 
 router.put('/:itemId', authenticateJWT, (req, res, next) => itemsController.updateTheWholeItem(req, res, next))
 
